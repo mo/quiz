ac662c13c9d43fa84103df899be150b8bc1e806b
Martin Olsson <martin@minimum.se>
2015-06-14 23:37:50 +0200

    Delete some cruft

--- pages/setup_page.php
+++ pages/setup_page.php
@@ -1,21 +1,18 @@
 <h1 class="qm_headline">QuizMaster - Setup</h1>
 
-<p class="qmText">QuizMaster needs access to a MySQL database to store quiz data, scores etc, 
+<p class="qmText">QuizMaster needs access to a MySQL database to store quiz data, scores etc,
    but the MySQL database host/login has not been configured yet. You don't have to create any
-   tables in the database, just tell QuizMaster where it is; once given access to the database 
-   QuizMaster will install itself properly. The database login credentials will be stored 
-   inside the file quizmaster/config/config.php so that QuizMaster can login to the database
+   tables in the database, just tell QuizMaster where it is; once given access to the database
+   QuizMaster will install itself properly. The database login credentials will be stored
+   inside the file prefs/settings.php so that QuizMaster can login to the database
    later on without the need to repeat this setup stage.</p>
-<p class="qmText">QuizMaster has a lot of other things you can doodle with in order to make your 
-   installation more personal, for instance themes and so on, however only the first four fields
-   are mandatory.</p>
 <?php
   if (isset($GLOBALS['error_message'])) {
     echo "<p class=\"qm_bad\">" . $GLOBALS['error_message'] . "</p>";
   }
-  
 
-  # If the form values are already set, reuse existing values. Otherwise, use the 
+
+  # If the form values are already set, reuse existing values. Otherwise, use the
   # values specified in SETTINGS_FILE (or default values if there is no such file)
   # The form values will already be set, for instance if this a theme preview form
   # or if the form was submitted with invalid data (and therefore now is being
@@ -25,7 +22,7 @@
   if (!isset($form_mysql_username)) $form_mysql_username = MYSQL_USERNAME;
   if (!isset($form_mysql_password)) $form_mysql_password = MYSQL_PASSWORD;
   if (!isset($form_admin_email)) $form_admin_email = ADMIN_EMAIL;
-    
+
 ?>
 
 <form method="post" action="?action=setup_exec">
@@ -34,7 +31,7 @@
     <label for="form_mysql_hostname">MySQL Hostname</label>
     <input name="form_mysql_hostname" type="text" value="<?php echo $form_mysql_hostname ?>" class="qmFormText"/>
   </div>
-  
+
   <div class="qmFormRow">
     <label for="form_mysql_database">MySQL Database</label>
     <input name="form_mysql_database" type="text" value="<?php echo $form_mysql_database ?>" class="qmFormText" />
@@ -44,7 +41,7 @@
     <label for="form_mysql_username">MySQL Username</label>
     <input name="form_mysql_username" type="text" value="<?php echo $form_mysql_username ?>" class="qmFormText"/>
   </div>
-  
+
   <div class="qmFormRow">
     <label for="form_mysql_password">MySQL Password</label>
     <input name="form_mysql_password" type="password" value="<?php echo $form_mysql_password ?>" class="qmFormText" />
@@ -62,14 +59,14 @@
         $file = substr($file, 0, strlen($file) - 4);
         echo('      <option' . (($file == get_current_theme())?' selected':'') . '>' .
              $file . '</option>' . "\n");
-      }  
-    } 
+      }
+    }
     closedir($handle);
-   }              
+   }
 ?>
     </select>
   </div>
-  
+
   <div class="qmFormRow">
     <input type="submit" name="btnSubmit" value="PREVIEW THEME" class="qmFormSubmit" />
   </div>
@@ -86,5 +83,5 @@
   <div class="qmFormRow">
     <input type="submit" name="btnSubmit" value="SAVE SETUP" class="qmFormSubmit" />
   </div>
-  
+
 </form>
