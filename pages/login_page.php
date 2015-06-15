61436d4a3b3a7139f8410b4889c455f2ac1c7fda
Martin Olsson <martin@minimum.se>
2015-06-15 23:18:47 +0200

    Fix grammar and don't mention features that don't exist

--- pages/login_page.php
+++ pages/login_page.php
@@ -1,11 +1,10 @@
 <h1 class="qm_headline">QuizMaster - Login</h1>
 
-<p class="qmText">Welcome to the quiz site. Here you can take different quizzes created by other members of
-the quiz site, but you can also create your own quizzes, privately or publicly for anyone to take. Additionally,
-the site assembles some basic statistics like the number of times each quiz has been taken etc. You are free to
-use the quiz site for anything you like, general trivia, homework training or even as a survey tool.</p>
+<p class="qmText">Welcome to the QuizMaster site. Here you can take different quizzes created by other members of
+the quiz site, but you can also create your own quizzes, privately or publicly for anyone to take. You are free to
+use the quiz site for anything you like, general trivia or homework training etc.</p>
 
-<p class="qmText">Enter your username and password in the text boxes below. If you are new to this site and is
+<p class="qmText">Enter your username and password in the text boxes below. If you are new to this site and are
 not a registered user you may use this form to <a href="?action=user_create">create a new account</a>.
 
 <p class="qmText">If you login as guest, by clicking the button at the bottom of this page, you will not be
